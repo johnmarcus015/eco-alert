9ec13e9f54dc18ecfe2df39f1c2d1e03719b3a59
johnmarcus015 <johnmarcus015@gmail.com>
2024-11-30 21:18:24 -0300

    Fixes gradle settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "br.com.ecoalert"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "br.com.ecoalert"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
